0f034a5738de3e1d17e3f96a3338abbd8f8d8dc1
Tyler Wales <walestyl@gmail.com>
2017-09-30 20:18:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,10 +21,8 @@ function sayHiToGrandma(string) {
     return "I can't hear you!";
   }
   else if (string === string.toUpperCase()) {
-    return "YES INDDEED!"
-    
+    return "YES INDDEED!";
+
   }
-    
+
 }
-
-
